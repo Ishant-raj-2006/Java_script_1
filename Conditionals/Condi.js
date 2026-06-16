a372ce40cdc0f247e3aed9b8490525f163008e6e
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-16 11:20:37 +0530

    Update 1.1

---
+++ Conditionals/Condi.js
@@ -0,0 +1,12 @@
+// WAP for find even number or not 
+alert("JS File Loaded")
+let num = prompt("Enter Number ");
+console.log(num);
+if(num%2==0){
+    console.log("Even Number ");
+    document.write("Even Number ");
+}
+else{
+    console.log("Odd Number ");
+    document.write("Odd Number");
+}
---
+++ Conditionals/condi1.js
@@ -0,0 +1,7 @@
+let x = 10;
+if(x>10){
+    console.log("X is greater then 10");
+}
+else{
+    console.log("X is less then 10");
+}
