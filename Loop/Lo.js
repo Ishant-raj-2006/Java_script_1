4dec6f576b787e0de6d3aeba5c6b37cbb6f70b66
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-18 09:56:34 +0530

    Today 18/06/2026

---
+++ Loop/Lo.js
@@ -0,0 +1,8 @@
+console.log("In this Lecture we learn Loops  \n there are 6 types of Loops \n 1. For Loop. \n 2. While Loop. \n 3. do - while Loop. \n 4. for in Loop. \n 5. for-of Loop \n 6. for each Loop.");
+
+console.log("Loops basicaly use to check any condition or repeated work ");
+
+// WAP for Print the your name at 100 times.
+for(let i=1; i<=100; i++){
+    console.log(i+" Ishant Raj ");
+}
