a6010709b559f792ba7ea72940c26a58d9902f71
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-20 09:38:48 +0530

    Today 20/06/2026

---
+++ Functions/fun2.js
@@ -0,0 +1,14 @@
+// WAP for greet just Hello Ishant by using return function.
+function greet(name){
+    return `Hello ${name}`;
+}
+let result = greet("Ishant");
+console.log(result);
+
+
+function greet(name){
+    return `Hello ${name}`;
+}
+console.log(greet("Ishant Raj"));
+
+
