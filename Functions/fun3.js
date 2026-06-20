cf3c03cd31e1fc205e207d557c7550468bcf1069
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-06-20 09:49:10 +0530

    This is Function

---
+++ Functions/fun3.js
@@ -0,0 +1,7 @@
+// We create a arrow functin 
+let greet = () => console.log(`Ishant Raj `);
+greet();
+
+// WAP for find the sum of Two n numbers by using Arrow Function.
+let sum = (a,b) => console.log(a+b);
+sum(10,200);
